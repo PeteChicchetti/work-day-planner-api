ff46f5eb7a17b69808f887b6d00061755ced6351
PeteChicchetti <pj0524@gmail.com>
2022-10-08 14:58:03 -0400

    edit to html-page fully functional

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -21,7 +21,6 @@ function hourTracker() {
     var currentHour = moment().format("H");
     $('.time-block').each(function() {
         var eventHour = parseInt($(this).attr('id').split('hour')[1]);
-        console.log(eventHour, currentHour)
 
         if(eventHour < currentHour) {
             $(this).addClass('past');
@@ -45,3 +44,20 @@ hourTracker();
 
 
 // Save item button
+$('.saveBtn').on('click', function() {
+
+    var textInput = $(this).siblings('.description').val();
+    var time = $(this).parent().attr('id');
+    localStorage.setItem(time, textInput);
+
+});
+
+$('#hour9 .description').val(localStorage.getItem('hour9'));
+$('#hour10 .description').val(localStorage.getItem('hour10'));
+$('#hour11 .description').val(localStorage.getItem('hour11'));
+$('#hour12 .description').val(localStorage.getItem('hour12'));
+$('#hour13 .description').val(localStorage.getItem('hour13'));
+$('#hour14 .description').val(localStorage.getItem('hour14'));
+$('#hour15 .description').val(localStorage.getItem('hour15'));
+$('#hour16 .description').val(localStorage.getItem('hour16'));
+$('#hour17 .description').val(localStorage.getItem('hour17'));
